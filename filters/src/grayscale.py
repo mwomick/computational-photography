6b882b8c685e94829062523db998f0ff7cbb073c
mwomick <michael@womick.me>
2021-02-12 21:20:10 -0500

    casting

--- filters/src/grayscale.py
+++ filters/src/grayscale.py
@@ -1,16 +1,38 @@
 from skimage import color
 import numpy as np
 
-def built_in_method(im):
-    return color.rgb2gray(color.rgba2rgb(im))
 
-def formula_method(im):
+def built_in_method(im):
+    return color.rgb2gray(im)
+
+
+def average_method(im):
     height, width, channels = im.shape
     imo = np.zeros((height, width, 3), dtype=int)
 
     for y in range(height):
         for x in range(width):
-            gray = im[y][x][0] * 0.299 + im[y][x][1] * 0.587 + im[y][x][2] * 0.114
+            gray = int((float(im[y][x][0]) + float(im[y][x][1]) + float(im[y][x][2]))/3.0)
             imo[y][x] = (gray, gray, gray)
 
     return imo
+
+
+def weighted_method(im):
+    height, width, channels = im.shape
+    imo = np.zeros((height, width, 3), dtype=int)
+
+    for y in range(height):
+        for x in range(width):
+            gray = int(im[y][x][0] * 0.299 + im[y][x][1] * 0.587 + im[y][x][2] * 0.114)
+            imo[y][x] = (gray, gray, gray)
+
+    return imo
+
+
+def hsv_method(im):
+    height, width, channels = im.shape
+    imo = np.zeros((height, width, 3), dtype=int)
+
+    for y in range(height):
+        for
