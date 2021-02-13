2834aff54fbf0fab6f0a59d04d830e798bf783a5
mwomick <michael@womick.me>
2021-02-12 21:20:16 -0500

    casting

--- filters/run.py
+++ filters/run.py
@@ -1,5 +1,6 @@
 from skimage import io
 import numpy as np
+from skimage import img_as_ubyte
 
 from src import grayscale as gra
 
@@ -8,13 +9,14 @@ OUTPUT_ROOT_DIR = 'data/out/'
 
 
 def gray():
-    ims = ['belltower.png', 'venice1.jpg']
+    ims = ['belltower.jpg', 'venice1.jpg']
 
     for f in ims:
         im = io.imread(INPUT_ROOT_DIR + f)
         imo = gra.built_in_method(im)
-        io.imsave(OUTPUT_ROOT_DIR + "exercise1/method1/" + f, gra.built_in_method(im))
-        io.imsave(OUTPUT_ROOT_DIR + "exercise1/method2/" + f, gra.formula_method(im))
+        io.imsave(OUTPUT_ROOT_DIR + "exercise1/method1/" + f, img_as_ubyte(gra.built_in_method(im)))
+        io.imsave(OUTPUT_ROOT_DIR + "exercise1/method2/" + f, img_as_ubyte(gra.average_method(im)))
+        io.imsave(OUTPUT_ROOT_DIR + "exercise1/method3/" + f, img_as_ubyte(gra.weighted_method(im)))
 
 
 gray()
