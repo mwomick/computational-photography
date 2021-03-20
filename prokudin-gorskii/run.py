f3a6260e636748a5e1fa0a3405e969e497fd5a9d
Michael Womick <rwomick@live.unc.edu>
2021-03-20 11:28:57 -0400

    nearly done w cropping

--- prokudin-gorskii/run.py
+++ prokudin-gorskii/run.py
@@ -8,20 +8,29 @@ from skimage import img_as_ubyte
 import os
 
 from src.autocrop import crop
-from src.align_basic import align_basic as basic 
+# from src.align_basic import align_basic as basic 
 
 INPUT_ROOT_DIR = 'data/in/'
 OUTPUT_ROOT_DIR = 'data/out/' 
 
 def preprocess(im):
-    pass
+    height, width  = im.shape
+    b = im
+    g = im[int(height/4):height, 0:width]
+    r = im[int(height/2):height, 0:width]
+    b = crop(b)
+    g = crop(g)
+    r = crop(r)
+    return((r, g, b))
+
 
 def align_basic():
-    ims = ['camel.jpg']
+    ims = ['cathedral.jpg', 'camel.jpg', 'chapel.jpg', 'courtyard.jpg', 'emir.jpg', 'gruppa.jpg', 'khan.jpg', 'monastery.jpg', 'nativity.jpg', 'railroad.jpg', 'settlers.jpg', 'urn.jpg']
 
     for f in ims:
         im = io.imread(INPUT_ROOT_DIR + f)
-        io.imsave(OUTPUT_ROOT_DIR + "part1/" + f, img_as_ubyte(basic(im)))
+        r, g, b = preprocess(im)
+        # io.imsave(OUTPUT_ROOT_DIR + "part1/" + f, img_as_ubyte(crop(im)))
 
 
 def clean():
--- prokudin-gorskii/src/autocrop.py
+++ prokudin-gorskii/src/autocrop.py
@@ -1,2 +1,60 @@
+from PIL import Image
+from cv2 import Sobel
+from cv2 import CV_64F
+import cv2
+import numpy
+
+# == DEBUGGING =========
+from matplotlib import pyplot as plt
+# ======================
+
 def crop(im):
-    pass
+    height, width  = im.shape
+
+    sobelx = Sobel(im, CV_64F, 1, 0, ksize=5)
+    sobely = Sobel(im, CV_64F, 0, 1, ksize=5)
+
+    size = 31
+    kernel_blur_x = numpy.zeros((size, size))
+    kernel_blur_x[int((size-1)/2), :] = numpy.ones(size)
+    kernel_blur_x = kernel_blur_x / size
+
+    kernel_blur_y = numpy.zeros((size, size))
+    kernel_blur_y[:, int((size-1)/2)] = numpy.ones(size)
+    kernel_blur_y = kernel_blur_y / size
+  
+    sobely = cv2.filter2D(sobely, -1, kernel_blur_x)
+    sobelx = cv2.filter2D(sobelx, -1, kernel_blur_y)
+
+    sobely = numpy.multiply(sobely, sobely)
+    sobelx = numpy.multiply(sobelx, sobelx)
+
+    left = 0
+    for i in range(0, 200):
+        if(numpy.mean(sobelx[:, i]) > 1.5e7):
+            left = i
+            break
+
+    right = 0
+    for i in range(width-1, 200, -1):
+        if(numpy.mean(sobelx[:, i]) > 1.5e7):
+            right = i
+            break 
+
+    top = 0
+    for i in range(0, height):
+        if(numpy.mean(sobely[i, :]) > 1.5e7):
+            top = i
+            break
+
+    bottom = height
+    for i in range(top+250, height):
+        if(numpy.mean(sobely[i, :]) > 1.5e7):
+            bottom = i
+            break
+
+    plt.imshow(im[top:bottom, left:right])
+    # plt.imshow(sobely)
+    plt.show()
+
+    return im[top:bottom, left:right]
