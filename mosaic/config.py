c0cc94a85ea8fc512fe3cfaabc6215432b23fa8e
Michael Womick <rwomick@live.unc.edu>
2021-04-29 05:20:51 -0400

    bokeh

--- mosaic/config.py
+++ mosaic/config.py
@@ -1,5 +1,5 @@
 DATASET_ROOT_DIR = 'data/in/val2014/'
-DATASET_MAX = 25000
+DATASET_MAX = 10000
 OUTPUT_ROOT_DIR = 'data/out/'
-TEMPLATE_PATH = 'data/in/templates/paris.jpg'
+TEMPLATE_PATH = 'data/in/templates/profile.png'
 MAX_THREADS = 150
