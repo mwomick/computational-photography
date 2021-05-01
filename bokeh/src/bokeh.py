9203446555eedbd1bf38388b83252168224b7d83
Michael Womick <rwomick@live.unc.edu>
2021-05-01 17:27:25 -0400

    bokeh done

--- bokeh/src/bokeh.py
+++ bokeh/src/bokeh.py
@@ -4,28 +4,36 @@ import numpy as np
 from scipy import signal
 import cv2 as cv
 
-def bokeh(image, mask):
-    dst = np.zeros((image.shape[0], image.shape[1], 3), dtype=int)
-    fore = np.array(image*((255-mask)/255), dtype=int)
-    back = np.array(image*(mask/255), dtype=int)
-    
-    r = 10
+def disk_filter(r):
     y,x = np.ogrid[-r: r+1, -r: r+1]
     disk = x**2+y**2 <= r**2
-    disk = disk.astype(float)/(3.14*r*r)
+    return(disk.astype(float)/(3.14*r*r))
+
+def bokeh(image, mask):
+    dst = np.zeros((image.shape[0], image.shape[1], 3), dtype=float)
+    fore = np.array(image*((255-mask)/(255*255)), dtype=float)
     
+    back = np.array(image*(mask/(255)), dtype=float)
 
+    disk = disk_filter(50)
+    back_blurred = cv.filter2D(back, 0, disk)/255.
+    mask_blurred = cv.filter2D(mask, 0, disk)/255.
 
-    blurred = cv.filter2D(back, 0, disk)
+    e = np.nan_to_num(np.divide(back_blurred, mask_blurred))
+    
+    for y in range(fore.shape[0]):
+        for x in range(fore.shape[1]):
+            if mask[y,x].all() == 0:
+                e[y,x] = fore[y,x]
 
-    plt.imshow(blurred+fore)
+    plt.imshow(e)
+
     plt.show()
 
 INPUT_ROOT_DIR = 'data/in/'
 OUTPUT_ROOT_DIR = 'data/out/' 
 
-
-image = io.imread(INPUT_ROOT_DIR + "couple.png")[:,:,:3]
-mask = io.imread(INPUT_ROOT_DIR + "couple_mask.png")[:,:,:3]
+image = io.imread(INPUT_ROOT_DIR + "building.png")[:,:,:3]
+mask = io.imread(INPUT_ROOT_DIR + "building_mask.png")[:,:,:3]
 
 bokeh(image, mask)
